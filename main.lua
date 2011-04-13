422d4bc82864f2d0e708358a6d0089e049e1a17b
Sam Fries <sam@friesart.com>
2011-04-12 22:39:00 -0700

    Nightly checkin- Compiles and runs, but tehre are plenty of issues (documented in README)

--- main.lua
+++ main.lua
@@ -1,35 +1,64 @@
 require "sprite"
+
+
+display.setStatusBar(display.HiddenStatusBar )
+--[[local tile= sprite.newSpriteSheet("images/brick_tiles_1.png",32 , 32)
+local sp=sprite.newSpriteSet(tile,1,1);
+local sp2=sprite.newSprite(sp);
+local q;
+]]--
+
+--Initialize info for Map loading
+	--Base string of the map location
+local mapstr="images/maps/";
+	--The current zone- naming convention is zone_ROOM#.png
+local zonestr="tut_";
+	--Current room number- starts with 1.
+local room=1;
+	--Convenience string.
+local ext=".png";
+
+
+--Now whenever we load a map, the file will be mapstr..zonestr..room..ext  (See how clever I am?)
+--Default the location to 0,0, then move it based on width so that it's appropriately located.
+local map=display.newImage(mapstr..zonestr..room..ext,0,0,true )
+
+
+
+--Acceleration trackers
+local xaccel=0;
+local yaccel=0;
+local poll=0;
+
+
+
+local cards={};
+for q=1, 4, 1 do
+	cards[q]=display.newImage("images/Card.png",64, 96,true);
+end
+
+cards[1].y=32;
+cards[2].y=32;
+cards[3].y=display.contentHeight - 32
+cards[4].y=display.contentHeight-32
+cards[1].x=48
+cards[2].x=display.contentWidth-48
+cards[3].x=display.contentWidth-48
+cards[4].x=48
+
 b=display.newImage("images/Button.png",200,300, true)
 bd=display.newImage("images/ButtonDown.png",200,300, false)
 bd.isVisible=false;
-local tile= sprite.newSpriteSheet("images/brick_tiles_1.png",32 , 32)
-local sp=sprite.newSpriteSet(tile,1,1);
-local sp2=sprite.newSprite(sp);
-local tilelist ={};
-local q;
-for q=1,12,1 do 
-	local q2;
-	tilelist[q]={}
-	for q2=1,17,1 do
-		tilelist[q][q2]=sprite.newSprite(sp);
-		tilelist[q][q2].y=(q2-2)*32;
-		tilelist[q][q2].x=(q-2)*32;
-	end
-end
-
 --TO DO:
-	--Variables for accel data
-	--Migration of movement to update function
+	--Map movement
 	--Implementation of xml parser to interpret a map file
 
-img = display.newImage("images/p1.png",140,200, true )
+img = display.newImage("images/p1.png",160,240, true )
+img.x=160;
+img.y=240;
 
 t=display.newText("ohai", 100,300, nil, 30)
-
-sp2.x=200;
-sp2.y=200;
-t.text=sp2.x..","..sp2.y
-
+t.rotation=90;
 run = 0;
 calx=0;
 caly=0;
@@ -43,8 +72,10 @@ local buttonlistener = function (event)
 		run=0
 		b.isVisible=true
 		bd.isVisible=false
-		img.x=140
-		img.y=200
+		img.x=160
+		img.y=240
+		meX=32*9;
+		meY=32*9;
 	end
 	return true
 end
@@ -56,53 +87,56 @@ local myListener = function (event)
 
 		run=1 
 	end
+
 	if (event.name == "accelerometer") then
 		local x=event.xGravity
 		local y=event.yGravity
 		x=x-calx;
 		y=y-caly;
+		
+		xaccel=xaccel+x;
+		yaccel=yaccel+y;
+		poll= poll+1;
 
-
-
-		if (math.abs(x)>math.abs(y)) then
-			for i,v in ipairs(tilelist) do
-				for i2,v2 in ipairs(v) do
-					v2.x=v2.x+15*x
-					t.text="x"
-					if (x<0) then
-						v2:setReferencePoint(display.CenterLeftReferencePoint)
-						if(v2.x<0) then
-							t.text="on top"
-						end
-						v2:setReferencePoint(display.CenterReferencePoint)
-					else
-						v2:setReferencePoint(display.CenterRightReferencePoint)
-						if (v2.x>display.contentWidth) then
-
-							t.text="on bottom"
-						end
-						v2:setReferencePoint(display.CenterReferencePoint)
-					end
-				end
-			end
-		else
-			for i,v in ipairs(tilelist) do
-				for i2, v2 in ipairs(v) do
-					v2.y=v2.y-10*y
-					t.text="y"
-				end
-			end
-		end
 
 	end
 end
-
+local poppp=0;
 local function update()
-	
+	if (poll>0) then
+	local x=xaccel/poll;
+	if (math.abs(x)<.01)then
+		x=0;
+	end
+	local y=yaccel/poll;
+	if (math.abs(y)<.01)then
+		y=0;
+	end
+		if (math.abs(x)>math.abs(y)) then
+			--If the movement is within the bounds of the map, move the map
+			--If the movement is within the bounds of the screen, move the character
+			if (map.x-15*x-display.contentWidth/2>0 and map.x-15*x<map.width) then
+				map.x=map.x-15*x
+			else
+				img.x=img.x+15*x
+				t.text=""..x;
+			end
+		else
+			if (map.y+10*y>0 and map.y+display.contentHeight/2+10*x<map.height) then
+				map.y=map.y+10*y
+			else
+				img.y=img.y-10*y
+				t.text=""..y
+			end
+		end
+	end
+	xaccel=0;
+	yaccel=0;
+	poll=0;
 end
 
 bd:addEventListener("touch",buttonlistener)
 b:addEventListener("touch", buttonlistener)
-system.setAccelerometerInterval(50)
+system.setAccelerometerInterval(60)
 Runtime:addEventListener("accelerometer", myListener)
-
+timer.performWithDelay(33, update, 0);
